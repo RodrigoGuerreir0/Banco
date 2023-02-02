709ef4f7427bc2d4b6045589101c710453c7df9f
Rodrigo Guerreiro <rodrigoguerreiro217@gmail.com>
2023-02-02 11:46:43 -0300

    Add files via upload

--- Banco/ContaCorrente.cs
+++ Banco/ContaCorrente.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Banco;
@@ -9,9 +10,37 @@ public class ContaCorrente
     public string nome;
     public int agencia;
     public string conta;
-    public double saldo;
-    public void sacar(double valor)
+    public double saldo, transferir;
+    public double valortranferencia;
+    public bool sacar(double valor)
     {
-        this.saldo -= valor;
+        
+        if (this.saldo >= valor)
+        {
+            this.saldo -= valor;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Saldo insuficiente para o saque.");
+            return false;
+        }
+        
+    
+    }
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        valortranferencia = valor;
+         if (this.saldo >= valor) {
+            this.saldo -= valor;
+            contaDestino.saldo += valor;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Não foi possivel realizar a transferencia. O saldo é insficiente");
+            return false;
+
+        }
     }
 }
--- Banco/Program.cs
+++ Banco/Program.cs
@@ -11,25 +11,32 @@ namespace Banco
         static void Main(string[] args)
         {
             ContaCorrente conta2 = new ContaCorrente();
-            conta2.agencia = 51;
-            conta2.conta = "123542-X";
-            conta2.nome = "Astolfo";
-            conta2.saldo = 250.00; Console.WriteLine("O saldo anterior é: R$" + conta2.saldo);
-            conta2.sacar(137);
-            Console.WriteLine("O saldo depois do saque é: " + conta2.saldo);             /*ContaCorrente conta1 = new ContaCorrente();
-            conta1.agencia = 51;
-            conta1.conta = "123542-X";
-            conta1.nome = "Astolfo";
-            conta1.saldo = 250.00;
-             Console.WriteLine("O saldo anterior é: R$" + conta1.saldo);
-            conta1.Depositar(5000);
-            Console.WriteLine("O saldo depois do deposito é: R$" + conta1.saldo);
-            /*             /*
-            Console.WriteLine("nome do usuario " + conta1.nome);
-            Console.WriteLine("sua agencia " + conta1.agencia);
-            Console.WriteLine("sua conta é " + conta1.conta);
-            Console.WriteLine("o saldo da conta é: R$" + conta1.saldo);
-            */
+            conta2.agencia = 4892;
+            conta2.conta = "260198-L";
+            conta2.nome = "Lucas";
+
+            conta2.saldo = 4000; 
+           // Console.WriteLine("O saldo anterior é: R$" + conta2.saldo);
+           //if (conta2.sacar(1000))
+           // Console.WriteLine("O saldo depois do saque é: " + conta2.saldo);
+
+         
+
+            ContaCorrente conta1 = new ContaCorrente();
+            conta1.agencia = 1500;
+            conta1.conta = "160606-R";
+            conta1.nome = "Rodrigo";
+            
+            conta1.saldo = 200;
+
+
+            Console.WriteLine("SALDO ATUAL DA CONTA QUE IRÁ TRANSFERIR: " + conta2.saldo);
+            Console.WriteLine("SALDO ATUAL DA CONTA QUE IRÁ RECEBER: " + conta1.saldo);
+
+            if (conta2.Transferir(20, conta1))
+                Console.WriteLine("SALDO ATUAL APÓS VOCÊ TRANSFERIR: " + conta2.saldo);
+                Console.WriteLine("SEU SALDO APOS RECEBER A TRANSFERENCIA " +conta1.saldo);
+            Console.WriteLine("O VALOR DA TRANSFERENCIA FOI DE: "+conta2.valortranferencia);
         }
     }
 }
